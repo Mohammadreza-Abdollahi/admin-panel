d5d1dddb2725548e2db907369c1b6b8aafdeccb1
Mohammadreza Abdollahi <mohammad1384abdollahi@gmail.com>
2024-09-28 23:38:41 +0330

    version 0.2.9
    fixed main.css bug...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './assets/css/index.css';
+import './assets/css/content.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
